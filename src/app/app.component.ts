862178a16a1253439a370516379bf6c319cf1095
UbaidMannethodi <ubaid@retailcloud.com>
2024-08-06 15:01:18 +0530

    Header and loader component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./shared/components/header/header.component";
+import {LoadingSpinnerComponent} from "./shared/components/loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LoadingSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  templateUrl: './loading-spinner.component.html',
+  standalone: true,
+  styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
