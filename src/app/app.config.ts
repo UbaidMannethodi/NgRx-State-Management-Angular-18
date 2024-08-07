ee9d8b49ed89d0c2eb187762b023427985e12330
UbaidMannethodi <ubaid@retailcloud.com>
2024-08-07 16:19:56 +0530

    Store dev tool added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import {provideStore} from "@ngrx/store";
 import {appReducer} from "./store/app.state";
+import {environment} from "./environment/environment";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideStore(appReducer),
+    provideStoreDevtools({
+      // maxAge: 25, // Retains last 25 states
+      logOnly: environment?.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true // If set to true, the connection is established within the Angular zone
+    })
   ]
 };
